c571cd27416b6a47c2230e4b0a34c835b7db9129
DanielCruz <dcruz.js@gmail.com>
2026-05-03 13:03:25 -0600

    Ejercicios 1.3-1.5

--- Informacion-del-Curso/src/App.jsx
+++ Informacion-del-Curso/src/App.jsx
@@ -17,52 +17,47 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      {props.parts.map((part) => (
+        <Part key={part.name} part={part.name} exercises={part.exercises} />
+      ))}
     </div>
   );
 };
 
 const Total = (props) => {
-  return (
-    <p>
-      Numero de ejercicios totales:{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
-    </p>
-  );
+  return <p>Numero de ejercicios totales: {props.exercises}</p>;
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  const { name, parts } = course;
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
+      <Header course={name} />
+      <Content parts={parts} />
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises={parts.reduce((total, part) => total + part.exercises, 0)}
       />
     </div>
   );
 };
 
 export default App;
-
-
-https://fullstackopen.com/es/part1/java_script
