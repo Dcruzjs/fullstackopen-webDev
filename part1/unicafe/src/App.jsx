a7e41ea7e0e5a192383303fba353c6c123d6ba4c
DanielCruz <dcruz.js@gmail.com>
2026-05-07 21:09:52 -0600

    Ejercicio 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 const StatisticsLine = (props) => {
   const { text, value } = props;
   return (
-    <div>
-      {text} {value}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
@@ -22,20 +23,22 @@ const Statistics = (props) => {
   }
 
   return (
-    <div>
-      <StatisticsLine text="good" value={good} />
-      <StatisticsLine text="neutral" value={neutral} />
-      <StatisticsLine text="bad" value={bad} />
-      <StatisticsLine text="all" value={good + neutral + bad} />
-      <StatisticsLine
-        text="average"
-        value={(good - bad) / (good + neutral + bad)}
-      />
-      <StatisticsLine
-        text="positive"
-        value={(good / (good + neutral + bad)) * 100 + "%"}
-      />
-    </div>
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="all" value={good + neutral + bad} />
+        <StatisticsLine
+          text="average"
+          value={(good - bad) / (good + neutral + bad)}
+        />
+        <StatisticsLine
+          text="positive"
+          value={(good / (good + neutral + bad)) * 100 + "%"}
+        />
+      </tbody>
+    </table>
   );
 };
 
